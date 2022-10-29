0fdfb378b4f81c516f4972a71d60a8ff283b98c9
AMAN SHARMA <91001908+sharma2095@users.noreply.github.com>
2022-10-29 14:03:53 +0530

    Add files via upload

--- flightapp/src/App.js
+++ flightapp/src/App.js
@@ -1,11 +1,20 @@
+import React,{useState} from "react";
+
 import logo from "./logo.svg";
 import "./App.css";
 import Register from "./Register";
+import {login} from "./login";
 
 function App() {
+  const [cuurentForm,setCurrentForm]=useState('login');
+
   return (
-    <div>
-      <Register />
+    
+    <div className="App">
+      {
+        currentForm=="Login"?<login/>:<Register/>
+      }
+      
     </div>
   );
 }
---
+++ flightapp/src/login.jsx
@@ -0,0 +1,24 @@
+
+import React,{useState} from "react";
+
+export const Login = () => {
+    const [email,setEmail]=useState('');
+    const[pass,setPass]=useState('');
+    const handleSubmit=(e)=>
+    {
+        e.preventDefault();
+        console.log(email);
+        }
+    return (
+        <>
+        <form onSubmit ={handleSubmit}>
+            <lable for="email">email</lable>
+            <input type="email" placeholder="enter email-id" id="email" name="email"/>
+            <lable for="email">password</lable>
+            <input type="password" placeholder="*********" id="password" name="password"/>
+            <button>Log In</button>
+           </form> 
+           <button>Don't have an account? register here.</button>
+           </>
+    )
+}
